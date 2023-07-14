ad5e27ecf436c85576f56e8dda71ed0a54fc948b
Santiago Calligari <santiago@calligari.ar>
2023-07-14 01:51:54 -0300

    Termine multimove

---
+++ README.md
@@ -0,0 +1,54 @@
+# multimove
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+
+`multimove` is a tool that moves files listed in a source file to a specified destination location.
+
+## Installation
+
+1. Clone this repository:
+
+```git clone https://github.com/santiagocalligari/multimove.git```
+
+
+2. Navigate to the project directory:
+
+```cd multimove```
+
+
+3. Build the project:
+
+```cargo install --path .```
+
+
+## Usage
+
+Run the program by specifying the path to the source file and the destination location:
+
+```multimove [OPTIONS] <filepath> <destination> ```
+
+The program will move all the files listed in the source file to the specified destination location.
+
+Additional options:
+
+- `-t, --threads <count>`: Specify the number of threads to use for moving the files. By default, all available threads will be used.
+
+## Examples
+
+- Move files using all available threads:
+
+```multimove <filepath> <destination> ```
+
+- Move files using only 2 threads:
+
+```multimove -t 2 <filepath> <destination> ```
+
+
+## Contribution
+
+Contributions are welcome. If you find any issues or have any suggestions, feel free to contribute.
+
+🔧👥 Enjoy moving your files with `multimove`! 😊🚚
+
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 use clap::{App, Arg};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-use std::io;
 use linecount::count_lines;
 use std::thread::available_parallelism;
 use tokio::task;
 use std::sync::Arc;
+use std::fs;
 
-
-fn move_files(thread_id: usize, filepath: &str, line_count: usize, thread_count: usize ){
-    println!("hola, las lineas que le corresponden al thread {} son: ", thread_id);
+fn move_files(thread_id: usize, filepath: &str, line_count: usize, thread_count: usize, destination : &str ){
     let mut to_read_lines = line_count/thread_count;
     if thread_id < line_count % thread_count {
         to_read_lines += 1;
@@ -18,12 +16,12 @@ fn move_files(thread_id: usize, filepath: &str, line_count: usize, thread_count:
     let reader = BufReader::new(File::open(filepath).expect("cannot open file"));
     let lines = reader.lines().collect::<Result<Vec<String>, _>>().unwrap();
 
-    //println!("La cantidad de lineas a leer son {},  las lineas son: {} los cores son {} ", to_read_lines, line_count, thread_count);
     for i in 0..to_read_lines{
         let to_read = thread_id + i*thread_count;
         if let Some(line) = lines.get(to_read)
         {
-            println!("Contenido de la linea {}: {} ", to_read, line);
+            let destiny = format!("{}/{}",destination.clone(), line);
+            let _ = fs::rename(line, destiny.clone());
         }
         else {
             println!("No se encontro la linea {} ", to_read);
@@ -43,6 +41,10 @@ async fn main() {
         .arg(Arg::with_name("filepath")
              .required(true)
              .help("you have to specify the file to be readed"))
+        .arg(Arg::with_name("destination")
+             .required(true)
+             .help("You have to specify the path where you want to move the files")
+             )
         .arg(Arg::with_name("threads")
              .short("t")
              .default_value(&binding)
@@ -63,16 +65,23 @@ async fn main() {
             .expect("No filepath provided")
             .to_owned(),
     );
+    let destination = Arc::new(
+        matches
+            .value_of("destination")
+            .expect("No filepath provided")
+            .to_owned(),
+    );
 
 
     let filepath_clone = Arc::clone(&filepath);
     let lines = count_lines(std::fs::File::open(&*filepath_clone).unwrap()).unwrap();
     for thread_id in 0..thread_count {
         let filepath_clone = Arc::clone(&filepath);
+        let destination_clone = Arc::clone(&destination);
 
         tokio::spawn( async move {
             {
-                move_files(thread_id, &filepath_clone, lines, thread_count);
+                move_files(thread_id, &filepath_clone, lines, thread_count, &destination_clone);
             }
             task::yield_now().await;
         } ).await.unwrap();
